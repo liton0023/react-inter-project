b72688cb036125d179d5dc2f3185742daac2278d
Md Liton Mahmud <litonmahmud0023@gmail.com>
2024-02-26 04:49:13 +0600

    comment code out of the component

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -6,71 +6,6 @@ import img from '/rs.png';
 const Product = () => {
     return (
       <div className="container">
-          {/* <div className=' w-full flex flex-4 justify-center'>
-       <div>
-       <span className=" w-full rounded-full bg-black">1</span>
-       </div>
-        <div className=' text-center'>
-            <img src={img} className="w-[141px] mx-auto" alt="" />
-            <p className='img-name'>Builder 1</p>
-            </div>  
-            <div className="text-center inline-block w-1/2">
-              <p className='discription'><span className='discription-heading'>WixPro 72-Inch Responsive Website Builder-</span>Comprehensive Digital Platform CreationvTool,Streamlined Design Interface for Professional Websites and Online Stores(Black/Blue)</p>
-              <span className='discription-heading'>Main Highlights</span>
-              <p className='main'>[What You Get]: Receive the WixPro website builder suite, access to premium design templates, an extensive library of widgets and apps, and detailed step-by-step guides.</p>
-              <button className='btn-2'>
-              <a href=""><span>Show more</span></a>
-                <IoChevronDownOutline />
-              </button>
-               </div>    
-         <div>      
-         <div className=""> 
-             <div className=" mx-auto">
-             <span className="reating">9.8</span>
-             <span> <LuAlertCircle /></span>
-             </div>
-             <span className="exceptional">Exceptional</span>
-             <span ><TiStarFullOutline className="star" /><TiStarFullOutline className="star" /><TiStarFullOutline className="star" /><TiStarFullOutline className="star" /><TiStarFullOutline className="star" />
-             </span>
-             </div>
-         <div  className=" mx-auto text-center">
-         <button>View</button>
-         </div>
-         </div> 
-        </div> */}
-          {/* <div className=' w-full flex flex-4 justify-center'>
-       <div>
-       <span className=" w-full rounded-full bg-black">1</span>
-       </div>
-        <div className=' text-center'>
-            <img src={img} className="w-[141px] mx-auto" alt="" />
-            <p className='img-name'>Builder 1</p>
-            </div>  
-            <div className="text-center w-1/2">
-              <p className='discription'><span className='discription-heading'>WixPro 72-Inch Responsive Website Builder-</span>Comprehensive Digital Platform CreationvTool,Streamlined Design Interface for Professional Websites and Online Stores(Black/Blue)</p>
-              <span className='discription-heading'>Main Highlights</span>
-              <p className='main'>[What You Get]: Receive the WixPro website builder suite, access to premium design templates, an extensive library of widgets and apps, and detailed step-by-step guides.</p>
-              <button className='btn-2'>
-              <a href=""><span>Show more</span></a>
-                <IoChevronDownOutline />
-              </button>
-               </div>    
-         <div>      
-         <div className=""> 
-             <div className=" mx-auto">
-             <span className="reating">9.8</span>
-             <span> <LuAlertCircle /></span>
-             </div>
-             <span className="exceptional">Exceptional</span>
-             <span ><TiStarFullOutline className="star" /><TiStarFullOutline className="star" /><TiStarFullOutline className="star" /><TiStarFullOutline className="star" /><TiStarFullOutline className="star" />
-             </span>
-             </div>
-         <div  className=" mx-auto text-center">
-         <button>View</button>
-         </div>
-         </div> 
-        </div>
-        */}
           <div className='product-container-2'>
        <div>
        <h5 className="number">1</h5>
@@ -139,38 +74,6 @@ const Product = () => {
          </div>
          </div> 
         </div>
-          {/* <div className='product-container-2'>
-       <div>
-       <h5 className="number">1</h5>
-       </div>
-        <div className='img'>
-            <img src={img} alt="" />
-            <p className='img-name'>Builder 1</p>
-            </div>  
-            <div className="product-item">
-              <p className='discription'><span className='discription-heading'>WixPro 72-Inch Responsive Website Builder-</span>Comprehensive Digital Platform CreationvTool,Streamlined Design Interface for Professional Websites and Online Stores(Black/Blue)</p>
-              <span className='discription-heading'>Main Highlights</span>
-              <p className='main'>[What You Get]: Receive the WixPro website builder suite, access to premium design templates, an extensive library of widgets and apps, and detailed step-by-step guides.</p>
-              <button className='btn-2'>
-              <a href=""><span>Show more</span></a>
-              <FaChevronDown></FaChevronDown>
-              </button>
-               </div>    
-         <div>      
-         <div className="review"> 
-             <div className="reating-div">
-             <li className="reating">9.8</li>
-             <li> <LuAlertCircle /></li>
-             </div>
-             <li className="exceptional">Exceptional</li>
-             <li ><TiStarFullOutline className="star" /><TiStarFullOutline className="star" /><TiStarFullOutline className="star" /><TiStarFullOutline className="star" /><TiStarFullOutline className="star" />
-             </li>
-             </div>
-         <div>
-         <button className="btn-3">View</button>
-         </div>
-         </div> 
-        </div> */}
       </div>
     );
 };
