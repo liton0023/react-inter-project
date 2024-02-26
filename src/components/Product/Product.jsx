79b97b20300aaebe7103f981ef675da4a4d80101
Md Liton Mahmud <litonmahmud0023@gmail.com>
2024-02-26 06:08:01 +0600

    ceware cdk section

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -1,4 +1,5 @@
 import { FaChevronDown } from "react-icons/fa";
+import { FaRegStarHalfStroke } from "react-icons/fa6";
 import { IoDiamond } from "react-icons/io5";
 import { LuAlertCircle } from "react-icons/lu";
 import { TiStarFullOutline } from "react-icons/ti";
@@ -51,11 +52,11 @@ const Product = () => {
        
           <div className='product-container-2'>
        <div>
-       <h5 className="number">1</h5>
+       <h5 className="number">2</h5>
        </div>
         <div className='img'>
             <img src={img} alt="" />
-            <p className='img-name'>Builder 1</p>
+            <p className='img-name'>Builder 2</p>
             </div>  
             <div className="product-item">
               <p className='discription'><span className='discription-heading'>WixPro 72-Inch Responsive Website Builder-</span>Comprehensive Digital Platform CreationvTool,Streamlined Design Interface for Professional Websites and Online Stores(Black/Blue)</p>
@@ -69,7 +70,41 @@ const Product = () => {
          <div>      
          <div className="review"> 
              <div className="reating-div">
-             <li className="reating">9.8</li>
+             <li className="reating">9.5</li>
+             <li> <LuAlertCircle /></li>
+             </div>
+             <li className="exceptional">Exceptional</li>
+            <div className="star-div">
+            <li ><TiStarFullOutline className="star" /><TiStarFullOutline className="star" /><TiStarFullOutline className="star" /><TiStarFullOutline className="star" /><FaRegStarHalfStroke className="star" />
+             </li>
+            </div>
+             </div>
+         <div>
+         <button className="btn-3">View</button>
+         </div>
+         </div> 
+        </div>
+          <div className='product-container-2'>
+       <div>
+       <h5 className="number">3</h5>
+       </div>
+        <div className='img'>
+            <img src={img} alt="" />
+            <p className='img-name'>Builder 3</p>
+            </div>  
+            <div className="product-item">
+              <p className='discription'><span className='discription-heading'>WixPro 72-Inch Responsive Website Builder-</span>Comprehensive Digital Platform CreationvTool,Streamlined Design Interface for Professional Websites and Online Stores(Black/Blue)</p>
+              <span className='discription-heading'>Main Highlights</span>
+              <p className='main'>[What You Get]: Receive the WixPro website builder suite, access to premium design templates, an extensive library of widgets and apps, and detailed step-by-step guides.</p>
+              <button className='btn-2'>
+              <a href=""><span>Show more</span></a>
+              <FaChevronDown />
+              </button>
+               </div>    
+         <div>      
+         <div className="review"> 
+             <div className="reating-div">
+             <li className="reating">9.3</li>
              <li> <LuAlertCircle /></li>
              </div>
              <li className="exceptional">Exceptional</li>
@@ -85,16 +120,30 @@ const Product = () => {
         </div>
           <div className='product-container-2'>
        <div>
-       <h5 className="number">1</h5>
+       <h5 className="number">4</h5>
        </div>
         <div className='img'>
             <img src={img} alt="" />
-            <p className='img-name'>Builder 1</p>
+            <p className='img-name'>CDK</p>
             </div>  
             <div className="product-item">
-              <p className='discription'><span className='discription-heading'>WixPro 72-Inch Responsive Website Builder-</span>Comprehensive Digital Platform CreationvTool,Streamlined Design Interface for Professional Websites and Online Stores(Black/Blue)</p>
+              <p className='discription'><span className='discription-heading'>CDK Resposive Builder:</span>An extensive library of widgets and apps, and detailed step-by-step guides</p>
+              <p className="offer">26% Off</p>
               <span className='discription-heading'>Main Highlights</span>
-              <p className='main'>[What You Get]: Receive the WixPro website builder suite, access to premium design templates, an extensive library of widgets and apps, and detailed step-by-step guides.</p>
+              <div className='main-4'>
+                <div className="main-div-4">
+                  <button className="main-4-btn">9.9</button>
+                  <p>building responsive</p>
+                </div>
+                <div className="main-div-4">
+                  <button className="main-4-btn">9.9</button>
+                  <p>Cool</p>
+                </div>
+                <div className="main-div-4">
+                  <button className="main-4-btn">9.9</button>
+                  <p>Docs</p>
+                </div>
+                 </div>
               <button className='btn-2'>
               <a href=""><span>Show more</span></a>
               <FaChevronDown />
@@ -103,7 +152,7 @@ const Product = () => {
          <div>      
          <div className="review"> 
              <div className="reating-div">
-             <li className="reating">9.8</li>
+             <li className="reating">9.3</li>
              <li> <LuAlertCircle /></li>
              </div>
              <li className="exceptional">Exceptional</li>
